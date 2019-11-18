9b9f9c0f5071272fb533a18197d26892b56f2a5d
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-19 04:14:08 +0900

    date string 변환

---
+++ stats/build/utils.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dateStringToDate = function (dateString) {
+    // 28/10/2018
+    var dateParts = dateString.split('/').map(function (v) {
+        return parseInt(v);
+    }); // ['28', '10', '2018']
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
