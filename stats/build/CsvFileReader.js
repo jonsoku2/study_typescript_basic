e1321556f459ea2fc8583ad912355a9565ca36e9
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-19 04:37:54 +0900

    stats -  Variable Types with Generics

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
+// T <-> TypeOfData
 var CsvFileReader = /** @class */ (function () {
     function CsvFileReader(filename) {
         this.filename = filename;
